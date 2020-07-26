169aa589a240de16e2ce1318ebcedd6d461d9e13
Derek Borski <derek.borski@gmail.com>
2020-07-26 14:10:21 -0600

    Build out news_api_service

---
+++ services/news_api_service.rb
@@ -0,0 +1,31 @@
+require 'faraday'
+require '././config/config'
+require 'pry'
+
+class NewsapiService
+
+  def get_everything(favorite_names, language_abbrev, sort_by, num_results)
+
+    params = { q: format_request(favorite_names), sortBy: sort_by, language: language_abbrev, pageSize: num_results}
+
+    get_json("v2/everything", params)
+  end 
+
+  private
+
+  def format_request(favorite_names)
+    favorite_names.join('"OR"')
+  end 
+
+  def conn
+    Faraday.new(url: "https://newsapi.org/") do |faraday|
+      faraday.headers["X-Api-Key"] = ENV["NEWS_API_KEY"]
+    end
+  end
+
+  def get_json(url, params = nil)
+    response = conn.get(url, params) do |request|
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end 

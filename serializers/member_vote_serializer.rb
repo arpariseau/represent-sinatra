7a9ed189de5f2ecacfa3d5c927ad364b2297d2c5
Alex Pariseau <arpariseau@gmail.com>
2020-07-30 17:32:14 -0600

    double equals versus equals is important

--- serializers/member_vote_serializer.rb
+++ serializers/member_vote_serializer.rb
@@ -16,7 +16,7 @@ class MemberVoteSerializer
   def find_member_vote
     member_vote = { @vote[:roll_call] => nil }
     @vote[:positions].each do |congress|
-      if congress[:member_id] = @member_id
+      if congress[:member_id] == @member_id
         member_vote[@vote[:roll_call]] = congress[:vote_position]
       end
     end
